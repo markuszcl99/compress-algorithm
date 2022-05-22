73d9d0e8de54fa9c53939594300b863545a2bcbc
markuszcl99 <e_markuszhang@163.com>
2022-05-22 15:26:31 +0800

    first commit

---
+++ src/main/java/com/markus/compress/utils/GzipUtils.java
@@ -0,0 +1,11 @@
+package com.markus.compress.utils;
+
+/**
+ * @author: markus
+ * @date: 2022/5/22 3:22 下午
+ * @Description: Gzip压缩工具
+ * @Blog: http://markuszhang.com/
+ */
+public class GzipUtils {
+
+}
